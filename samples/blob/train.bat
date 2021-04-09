020a2c7b3b0a3622b578f5a49b9a378c5a19329f
Ákos Köte <kote2ster@gmail.com>
2021-04-09 08:25:54 +0200

    Change run.bat's weight from coco to last

--- samples/blob/train.bat
+++ samples/blob/train.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 IF [%1] == [] (
     ECHO Using dataset from 'out' folder
-    blob.py train --dataset=out --weights=coco
+    blob.py train --dataset=out --weights=last
 ) ELSE (
     ECHO Using dataset from '%1' folder
-    blob.py train --dataset=%1 --weights=coco
+    blob.py train --dataset=%1 --weights=last
 )
