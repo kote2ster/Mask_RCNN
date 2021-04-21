028e56aae6665a73fdfa361d5e2d6d4d9929ef9c
Ákos Köte <kote2ster@gmail.com>
2021-04-21 11:19:27 +0200

    Add weights variable to train.bat

--- samples/blob/train.bat
+++ samples/blob/train.bat
@@ -3,6 +3,7 @@ IF [%1] == [] (
     ECHO Using dataset from 'out' folder
     blob.py train --dataset=out --weights=coco
 ) ELSE (
-    ECHO Using dataset from '%1' folder
-    blob.py train --dataset=%1 --weights=coco
+    ECHO Using dataset from %1 folder
+    ECHO Continuing from %2 dataset
+    blob.py train --dataset=%1 --weights=%2
 )
